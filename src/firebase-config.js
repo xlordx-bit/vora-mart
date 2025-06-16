caff54514dc888f0bc144c6403b9edc563933cd0
Pranjal AGARWAL <devilking0969@gmail.com>
2025-06-16 11:23:33 +0530

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDJPu6HFXJpDra05pmBgHaiHkx9L7Nhljg",
-  authDomain: "vora-mart.firebaseapp.com",
-  projectId: "vora-mart",
-  storageBucket: "vora-mart.firebasestorage.app",
-  messagingSenderId: "5365976566",
-  appId: "1:5365976566:web:4241c51dd1ba2b26010bd7",
-  measurementId: "G-74TX02FNJ7"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 
